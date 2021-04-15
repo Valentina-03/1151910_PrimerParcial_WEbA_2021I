71e09136618293f2eec685df8e2fb9ca717ba875
Paula Rico <paulavalentinarlin@ufps.edu.co>
2021-04-15 07:13:37 -0500

    Tercer Commit Agregando la barra lateral

--- js/consola.js
+++ js/consola.js
@@ -1,8 +1,20 @@
 function cargarDatos() {
-    var url = "https://carlosreneas.github.io/endpoints/noticias.json";
-    fetch(url)
+    var url1 = "https://carlosreneas.github.io/endpoints/noticias.json";
+    fetch(url1)
         .then(response => response.json())
         .then(data => generarNoticias(data))
+        .catch(error => console.log("Error: " + error.message));
+    
+    var url2 = "https://carlosreneas.github.io/endpoints/categoria_deporte.json";
+    fetch(url2)
+        .then(response => response.json())
+        .then(data => generarCategoriaDeporte(data))
+        .catch(error => console.log("Error: " + error.message));
+    
+    var url3 = "https://carlosreneas.github.io/endpoints/categoria_tecnologia.json";
+    fetch(url3)
+        .then(response => response.json())
+        .then(data => generarCategoriaTecnologia(data))
         .catch(error => console.log("Error: " + error.message));
 }
 
@@ -15,6 +27,30 @@ function generarNoticias(data) {
         var elemento = document.createElement("section");
         elemento.innerHTML = `<h3>${data[i].titulo}-${data[i].categoria}-${data[i].fecha}</h3>
         <p>${data[i].descripcion}. <a href="#">Ver más</a></p>`;
+        section.appendChild(elemento);
+    }
+}
+
+function generarCategoriaDeporte(data) {
+    var section = document.querySelector("#deportes");
+    var titulo = document.createElement("h4");
+    titulo.innerHTML = `<h4>Deportes</h4><br>`;
+    section.appendChild(titulo);
+    for (var i=0; i<3; i++) {
+        var elemento = document.createElement("section");
+        elemento.innerHTML = `<p>${data[i].titulo}</p><hr>`;
+        section.appendChild(elemento);
+    }
+}
+
+function generarCategoriaTecnologia(data) {
+    var section = document.querySelector("#tecnologia");
+    var titulo = document.createElement("h4");
+    titulo.innerHTML = `<h4>Tecnologia</h4><br>`;
+    section.appendChild(titulo);
+    for (var i=0; i<3; i++) {
+        var elemento = document.createElement("section");
+        elemento.innerHTML = `<p>${data[i].titulo}</p><hr>`;
         section.appendChild(elemento);
     }
 }
